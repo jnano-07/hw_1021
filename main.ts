63dda9ac0a6261622cf9a95e8573c283788b0f40
jnano-07 <71751213+jnano-07@users.noreply.github.com>
2020-10-28 14:29:14 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ basic.forever(function () {
         for (let i = 0; i <= 4; i++) {
             for (let j = 0; j <= 4; j++) {
                 if (i + j == index) {
-                    led.plot(4 - j, 4 - i)
+                    led.plot(4 - i, 4 - j)
                 }
             }
         }
